b202b6f115b57a92de82875268e4991276d113eb
coolerstuff10 <emmahmcardle@gmail.com>
2020-09-19 16:52:37 -0400

    Refactored code

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,9 +1,6 @@
 class TicTacToe
 
   def initialize
-  @board = [["1","2","3"],
-            ["4","5","6"],
-            ["7","8","9"]]
   @one = "1"
   @two = "2"
   @three = "3"
@@ -17,112 +14,61 @@ class TicTacToe
   @guess = "";
   @count = 0;
   end
-  
-  #players picks movement
-  #def playerChoice
-  #end
 
-  def beginning
+  def show_board
   #ask user first tile
-  puts "New game started... 
-  Player #{@playerNum}: choose your tile
+  puts "Player #{@playerNum}: choose your tile
+
    #{@one} | #{@two} | #{@three} 
   ___|___|____
    #{@four} | #{@five} | #{@six}
   ___|___|____
    #{@seven} | #{@eight} | #{@nine}  
      |   |    \n"
-  @guess = gets.chomp
-  guessing
+  puts " "
+  #players picks movement
+  @guess = gets.chomp.strip
   end 
 
   def guessing
-    case @guess
-    when "1"
-      if @one == "1"
-        @playerNum == 'one' ? @one = "0" : @one = "X"   
-        fillInBoard
-      else
-        taken
-      end       
-    when "2"
-      if @two == "2"
-        @playerNum == 'one' ? @two = "0" : @two = "X"   
-        fillInBoard
-      else
-        taken
-      end
-    when "3"
-      if @three == "3"
-        @playerNum == 'one' ? @three = "0" : @three = "X"   
-        fillInBoard
-      else
-        taken
-      end
-    when "4"
-      if @four == "4"
-        @playerNum == 'one' ? @four = "0" : @four = "X"   
-        fillInBoard
-      else
-        taken
-      end
-    when "5"
-      if @five == "5"
-        @playerNum == 'one' ? @five = "0" : @five = "X"   
-        fillInBoard
-      else
-        taken
-      end
-    when "6"
-      if @six == "6"
+    range = [@one, @two, @three, @four, @five, @six, @seven, @eight, @nine]
+    if range.include? @guess
+      @count += 1
+      case @guess
+      when "1"
+        @playerNum == 'one' ? @one = "0" : @one = "X"      
+      when "2"
+        @playerNum == 'one' ? @two = "0" : @two = "X"    
+      when "3"
+        @playerNum == 'one' ? @three = "0" : @three = "X"    
+      when "4"
+        @playerNum == 'one' ? @four = "0" : @four = "X" 
+      when "5"
+          @playerNum == 'one' ? @five = "0" : @five = "X"    
+      when "6"
         @playerNum == 'one' ? @six = "0" : @six = "X"   
-        fillInBoard
+      when "7"
+        @playerNum == 'one' ? @seven = "0" : @seven = "X"    
+      when "8"
+        @playerNum == 'one' ? @eight = "0" : @eight = "X"
+      when "9"          
+        @playerNum == 'one' ? @nine = "0" : @nine = "X" 
       else
-        taken
-      end
-    when "7"
-      if @seven == "7"
-        @playerNum == 'one' ? @seven = "0" : @seven = "X"   
-        fillInBoard
-      else
-        taken
-      end
-    when "8"
-      if @eight == "8"
-        @playerNum == 'one' ? @eight = "0" : @eight = "X"   
-        fillInBoard
-      else
-        taken
-      end
-    when "9"          
-      if @nine == "9"
-        @playerNum == 'one' ? @nine = "0" : @nine = "X"   
-        fillInBoard
-      else
-        taken
+        puts "You've entered the incorrect input!"  
+        show_board
       end
     else
-      puts "You've entered the incorrect input!"  
-      changePlayers
-      fillInBoard
+      puts "That tile is taken already! Please try another tile"
+      show_board
     end
+    return @count
   end  
 
-  def changePlayers
-    if @playerNum == 'one'
-      @playerNum = 'two'
-    else
-      @playerNum = 'one'
-    end 
+  def change_players
+    @playerNum == 'one' ? @playerNum = 'two' : @playerNum = 'one'
   end
 
-  def taken
-    puts "That tile is taken already! Please try another time"
-    changePlayers
-    fillInBoard
-  end
-
-  def fillInBoard
+  def check_if_won
     if @one == @two && @one == @three || @one == @four && @one == @seven ||
       @one == @five && @one == @nine || @three == @five && @three == @seven ||
       @two == @five && @two == @eight || @three == @six && @three == @nine ||
@@ -134,26 +80,20 @@ class TicTacToe
     ___|___|____
      #{@seven} | #{@eight} | #{@nine}  
        |   |    \n"     
-    elsif @count == 9
-      puts "There was a tie!"
-      exit  
-    else 
-    @count += 1 
-    puts "#{@count}" 
-    changePlayers   
-    puts "Player #{@playerNum}: Choose your tile
-     #{@one} | #{@two} | #{@three} 
-    ___|___|____
-     #{@four} | #{@five} | #{@six}
-    ___|___|____
-     #{@seven} | #{@eight} | #{@nine}  
-       |   |    \n"
-    @guess = gets.chomp
-    guessing
     end
   end  
 
-  game = TicTacToe.new
-  game.beginning
-
 end
+
+
+game = TicTacToe.new
+puts "New game started... "
+count = 0
+while count < 9
+  game.show_board
+  count = game.guessing
+  game.check_if_won
+  game.change_players
+end
+puts "There was a tie!"
+exit
